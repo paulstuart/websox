b431068b726ea051d71494d7ad445edfe7172481
Paul Stuart <pauleyphonic@gmail.com>
2017-11-16 16:40:32 -0800

    action now takes a reader, include Oauth2 security

--- client/main.go
+++ client/main.go
@@ -5,43 +5,68 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
-	"net/http"
 	"net/url"
+	"os"
 	"strings"
 
+	"github.com/gorilla/websocket"
 	"github.com/paulstuart/websox"
 )
 
 var (
-	addr = flag.String("addr", "localhost:8080", "http service address")
+	uaa_client_id     = os.Getenv("uaa_client_id")
+	uaa_client_secret = os.Getenv("uaa_client_secret")
+	uaa_url           = os.Getenv("uaa_url")
+	server_addr       = os.Getenv("server_addr")
 )
 
 func main() {
+	if len(server_addr) == 0 {
+		server_addr = "localhost:8080"
+	}
+
+	addr := flag.String("addr", server_addr, "http service address")
+	debug := flag.Bool("debug", false, "enable debugging")
 	flag.Parse()
+
 	log.SetFlags(0)
 
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/push"}
 	if strings.HasSuffix(*addr, "443") {
 		u.Scheme += "s"
 	}
-	headers := make(http.Header)
-	headers.Add("x-secret-code", "Open Sesame!")
-	websox.Client(u.String(), gotIt, headers)
+
+	if *debug {
+		fmt.Println("connecting to:", u.String())
+		fmt.Printf("CID: %s SECRET: %s URL: %s\n", uaa_client_id, uaa_client_secret, uaa_url)
+	}
+
+	ctx := context.Background()
+	for {
+		headers, err := websox.Oauth2Header(ctx, uaa_url, uaa_client_id, uaa_client_secret)
+		if err != nil {
+			panic(err)
+		}
+		err = websox.Client(u.String(), gotIt, headers)
+		if !websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+			fmt.Printf("(%T) %v\n", err, err)
+		}
+	}
 }
 
-var cnt int
-
-func gotIt(message []byte) error {
-	cnt++
-	fmt.Println("recv cnt:", cnt)
+func gotIt(r io.Reader) error {
 	var s websox.Stuff
-	if err := json.Unmarshal(message, &s); err != nil {
+	if err := json.NewDecoder(r).Decode(&s); err != nil {
+		fmt.Println("json error:", err)
 		return err
 	}
+	fmt.Println("Stuff count:", s.Count)
 	three := s.Count%3 == 0
 	five := s.Count%5 == 0
 	switch {
