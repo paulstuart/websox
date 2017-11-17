5f3e74c15b606494878a8f6627d4bfd96b6fe07b
Paul Stuart <pauleyphonic@gmail.com>
2017-11-16 16:34:15 -0800

    use zlib compression for sending messages

--- websox.go
+++ websox.go
@@ -7,6 +7,8 @@
 package websox
 
 import (
+	"bytes"
+	"compress/zlib"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -14,8 +16,10 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/pkg/errors"
 )
 
 // ErrMsg is for reporting errors on websocket pushes
@@ -31,49 +35,71 @@ func (e ErrMsg) Error() error {
 	return fmt.Errorf(e.Msg)
 }
 
-// Admin returns channels to get data and return the error when trying to save said data
-type Admin func() (chan interface{}, chan error)
+// Setup returns channels to get data and return the error when trying to save said data
+// If the setup function cannot do the required processing,
+// it should return a nil interface channel and send an error message in the error channel
+type Setup func() (chan interface{}, chan error)
 
-// Validator will validate requests to a web page
-type Validator func(*http.Request) error
-
-// Pusher will apply Admin functionality to a websocket server connection
-func Pusher(admin Admin, valid Validator) http.HandlerFunc {
+// Pusher gets send/recv channels from the setup function
+// and apply the channel data to a websocket connection
+func Pusher(setup Setup, expires *time.Duration) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if valid != nil {
-			if err := valid(r); err != nil {
-				http.Error(w, err.Error(), http.StatusForbidden)
-				return
-			}
+
+		// TODO: should we do origin checking?
+		//fmt.Printf("pusher origin:%s host:%s\n", r.Header.Get("Origin"), r.Host)
+
+		getter, teller := setup()
+		if getter == nil {
+			err := <-teller
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
-		fmt.Printf("pusher origin:%s host:%s\n", r.Header.Get("Origin"), r.Host)
-		getter, teller := admin()
+
 		upgrader := websocket.Upgrader{} // use default options
 		c, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
 			log.Print("push upgrade error:", err)
 			return
 		}
-		defer c.Close()
+
+		var stop time.Time
+		if expires != nil {
+			stop = time.Now().Add(*expires)
+		}
+
 		for {
+			if expires != nil && time.Now().After(stop) {
+				log.Println("timeout for duration:", *expires)
+				break
+			}
+
 			stuff, ok := <-getter
 			if !ok {
 				break
 			}
 			b, err := json.Marshal(stuff)
 			if err != nil {
-				log.Println("gusher json error:", err)
+				log.Println("pusher json error:", err)
+				teller <- err
 				continue
 			}
 
-			if err = c.WriteMessage(websocket.TextMessage, b); err != nil {
-				log.Println("gusher write error:", err)
+			// compress data before sending
+			var buff bytes.Buffer
+			z := zlib.NewWriter(&buff)
+			z.Write(b)
+			z.Close()
+
+			if err = c.WriteMessage(websocket.BinaryMessage, buff.Bytes()); err != nil {
+				log.Println("pusher write error:", err)
+				teller <- err
 				break
 			}
 
 			_, message, err := c.ReadMessage()
 			if err != nil {
-				log.Println("gusher read error:", err)
+				log.Println("pusher read error:", err)
+				teller <- err
 				break
 			}
 
@@ -86,28 +112,33 @@ func Pusher(admin Admin, valid Validator) http.HandlerFunc {
 
 			teller <- status.Error()
 		}
+		err = c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+		if err != nil {
+			log.Println("websocket server close error:", err)
+		}
+		c.Close()
 	}
 }
 
-// Actionable functions do something with a byte slice and return an error if such is encountered
-type Actionable func([]byte) error
+// Actionable functions process an io.Reader and return an error if such is encountered
+type Actionable func(io.Reader) error
 
 // Client will connect to url and apply the Actionable function to each message recieved
-func Client(url string, fn Actionable, headers http.Header) {
+func Client(url string, fn Actionable, headers http.Header) error {
 	log.Printf("connecting to %s", url)
 
 	c, resp, err := websocket.DefaultDialer.Dial(url, headers)
 	if err != nil {
 		io.Copy(os.Stdout, resp.Body)
-		log.Fatalf("dial code:%d status:%s error:%v\n", resp.StatusCode, resp.Status, err)
+		return errors.Wrapf(err, "dial code:%d status:%s", resp.StatusCode, resp.Status)
 	}
 	defer c.Close()
 
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
+	notify := make(chan os.Signal, 1)
+	signal.Notify(notify, os.Interrupt)
 
 	go func() {
-		what := <-interrupt
+		what := <-notify
 		log.Println("interrupt:", what)
 
 		// To cleanly close a connection, a client should send a close
@@ -116,19 +147,36 @@ func Client(url string, fn Actionable, headers http.Header) {
 		if err != nil {
 			log.Println("websocket close error:", err)
 		}
+		c.Close()
+		os.Exit(0)
 	}()
 
 	for {
-		_, message, err := c.ReadMessage()
+		messageType, r, err := c.NextReader()
 		if err != nil {
-			log.Println("websocket read error:", err)
-			return
+			return err
+		}
+
+		if messageType != websocket.BinaryMessage {
+			fmt.Printf("MSG (%T):", messageType)
+			io.Copy(os.Stdout, r)
+		}
+		if messageType == websocket.CloseMessage {
+			log.Println("got websocket close message")
+			return nil
+		}
+
+		// decompress the message before the function sees it
+		z, err := zlib.NewReader(r)
+		if err == nil {
+			err = fn(z)
 		}
 
 		var status ErrMsg
-		if err = fn(message); err != nil {
+		if err != nil {
 			status.Msg = err.Error()
 		}
+		z.Close()
 
 		b, err := json.Marshal(status)
 		if err != nil {
@@ -137,8 +185,8 @@ func Client(url string, fn Actionable, headers http.Header) {
 		}
 
 		if err := c.WriteMessage(websocket.TextMessage, b); err != nil {
-			log.Println("status write error:", err)
-			return
+			return errors.Wrap(err, "status write error")
 		}
 	}
+	return nil
 }
